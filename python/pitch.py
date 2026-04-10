570cd80cbe4bb6766569563729dbf7cadee1e392
dcjvliet <minestone4306@gmail.com>
2026-04-09 21:50:02 -0400

    made amplitude of pitch a closer approximation for loudness

--- python/pitch.py
+++ python/pitch.py
@@ -3,15 +3,18 @@ import numpy as np
 
 
 class Pitch:
-    def __init__(self, frequency: float, duration: float, sample_rate: int = 44100, amplitude: float = 1):
+    def __init__(self, frequency: float, duration: float, sample_rate: int = 44100, loudness: float = 1):
         self.frequency = frequency
         self.duration = duration
         self.sample_rate = sample_rate
-        self.amplitude = amplitude
+        if loudness < 0 or loudness > 1:
+            raise ValueError('Loudness must be between 0 and 1')
+        
+        self.loudness = loudness ** 1.66 # roughly account for how loud something sounds to humans
         self.audio = None
 
         # generate samples required to play the pitch
-        self.samples = (self.amplitude * np.sin(2 * np.pi * np.arange(self.sample_rate * self.duration) * self.frequency / self.sample_rate)).astype(np.float32)
+        self.samples = (self.loudness * np.sin(2 * np.pi * np.arange(self.sample_rate * self.duration) * self.frequency / self.sample_rate)).astype(np.float32)
 
     def play(self):
         # create audio if necessary
--- testing/pitch_test.py
+++ testing/pitch_test.py
@@ -3,12 +3,12 @@ from python.pitch import Pitch
 
 if __name__ == '__main__':
     # Create a 1 second 440Hz pitch and play it
-    pitch = Pitch(frequency=440.0, duration=1.0)
+    pitch = Pitch(frequency=440.0, duration=1, loudness=0.5)
     print(f"Generated {len(pitch.samples)} samples for 440Hz")
     pitch.play()
     pitch.cleanup()
 
     # Create a 1 second 440Hz pitch and play it at a quarter of the amplitude
-    quiet_pitch = Pitch(frequency=440.0, duration=1.0, amplitude=0.25)
+    quiet_pitch = Pitch(frequency=440.0, duration=1.0, loudness=1)
     quiet_pitch.play()
     quiet_pitch.cleanup()
