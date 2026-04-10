359129e387c4949429d64ea5f635dca8f330559f
dcjvliet <minestone4306@gmail.com>
2026-04-09 21:43:31 -0400

    made pitch test also test amplitude

--- testing/pitch_test.py
+++ testing/pitch_test.py
@@ -2,8 +2,13 @@ from python.pitch import Pitch
 
 
 if __name__ == '__main__':
-    # Test the Pitch class
-    pitch = Pitch(frequency=440.0, duration=2.0)
+    # Create a 1 second 440Hz pitch and play it
+    pitch = Pitch(frequency=440.0, duration=1.0)
     print(f"Generated {len(pitch.samples)} samples for 440Hz")
     pitch.play()
     pitch.cleanup()
+
+    # Create a 1 second 440Hz pitch and play it at a quarter of the amplitude
+    quiet_pitch = Pitch(frequency=440.0, duration=1.0, amplitude=0.25)
+    quiet_pitch.play()
+    quiet_pitch.cleanup()
