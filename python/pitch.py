4414cba702a326e4407a0097cedbdd162902c1f3
dcjvliet <minestone4306@gmail.com>
2026-04-09 21:29:27 -0400

    renamed pitch to frequency

--- python/pitch.py
+++ python/pitch.py
@@ -3,15 +3,15 @@ import numpy as np
 
 
 class Pitch:
-    def __init__(self, pitch: float, duration: float, sample_rate: int = 44100, amplitude: float = 1):
-        self.pitch = pitch
+    def __init__(self, frequency: float, duration: float, sample_rate: int = 44100, amplitude: float = 1):
+        self.frequency = frequency
         self.duration = duration
         self.sample_rate = sample_rate
         self.amplitude = amplitude
         self.audio = None
 
         # generate samples required to play the pitch
-        self.samples = (self.amplitude * np.sin(2 * np.pi * np.arange(self.sample_rate * self.duration) * self.pitch / self.sample_rate)).astype(np.float32)
+        self.samples = (self.amplitude * np.sin(2 * np.pi * np.arange(self.sample_rate * self.duration) * self.frequency / self.sample_rate)).astype(np.float32)
 
     def play(self):
         # create audio if necessary
