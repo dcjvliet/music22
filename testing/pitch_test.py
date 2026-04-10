c68fc8af89c1da1c80ac178f6e0ea1b7380674f1
dcjvliet <minestone4306@gmail.com>
2026-04-10 09:47:29 -0400

    Added code for testing saving sounds to test files

--- testing/pitch_test.py
+++ testing/pitch_test.py
@@ -11,4 +11,7 @@ if __name__ == '__main__':
     # Create a 1 second 440Hz pitch and play it at a quarter of the amplitude
     quiet_pitch = Pitch(frequency=440.0, duration=1.0, loudness=1)
     quiet_pitch.play()
+    quiet_pitch.save_to_file('wav', 'quiet_pitch.wav')
+    quiet_pitch.save_to_file('ogg', 'quiet_pitch.ogg')
+    quiet_pitch.save_to_file('mp3', 'quiet_pitch')
     quiet_pitch.cleanup()
